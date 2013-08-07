ee402f10f375129e34afd2c288aa99a148180487
Mike Saparov <mike.saparov@mulesoft.com>
2013-08-06 18:13:22 -0700

    plugin - does not compile

--- src/main/java/com/anyconsole/db/MongoPlugin.java
+++ src/main/java/com/anyconsole/db/MongoPlugin.java
@@ -4,4 +4,16 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MongoPlugin implements Plugin {
+
+	@Override
+	public Parser parse(String statement) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String execute(Parser parser) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
