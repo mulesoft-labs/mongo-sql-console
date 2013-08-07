9c9bd56463ed5837996f946d5fc563f9155837e0
Konstantin Babushkin <kbabushkin@mulesoft.com>
2013-08-06 21:37:51 -0700

    fixed test

--- src/test/java/com/anyconsole/core/parser/SQLParserTest.java
+++ src/test/java/com/anyconsole/core/parser/SQLParserTest.java
@@ -1,6 +1,10 @@
 package com.anyconsole.core.parser;
 
+import com.anyconsole.db.Builder;
 import net.sf.jsqlparser.JSQLParserException;
+import net.sf.jsqlparser.schema.Table;
+import net.sf.jsqlparser.statement.select.PlainSelect;
+import net.sf.jsqlparser.statement.update.Update;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -24,12 +28,42 @@ public class SQLParserTest {
     @Test
     public void parseSelectTest() throws JSQLParserException {
         SQLParser parser = new SQLParser("SELECT * FROM table1");
-        parser.execute();
+        parser.execute(new Builder() {
+            @Override
+            public String getResult() {
+                return null;  //Implementation goes here
+            }
+
+            @Override
+            public void doSelect(PlainSelect plainSelect) {
+                //Implementation goes here
+            }
+
+            @Override
+            public void doUpdate(Table table, Update update) {
+                //Implementation goes here
+            }
+        });
     }
 
     @Test
     public void parseUpdateTest() throws JSQLParserException {
         SQLParser parser = new SQLParser("UPDATE table1 SET table1.column=value1");
-        parser.execute();
+        parser.execute(new Builder() {
+            @Override
+            public String getResult() {
+                return null;  //Implementation goes here
+            }
+
+            @Override
+            public void doSelect(PlainSelect plainSelect) {
+                //Implementation goes here
+            }
+
+            @Override
+            public void doUpdate(Table table, Update update) {
+                //Implementation goes here
+            }
+        });
     }
 }
